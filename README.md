7ba119767c9728bd480a0cd43a71f0ae8adda350
willi <willi168@163.com>
2021-05-08 11:03:52 +0800

    监听redis key失效，失效回调

---
+++ README.md
@@ -0,0 +1,2 @@
+### 参考文档
+[Python实现订单超时自动取消](https://blog.csdn.net/itcast_cn/article/details/93215416)
---
+++ redis_ex_sub.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# 基于redis过期key实现订单超时取消，返还库存
+# vim /etc/redis/redis.conf
+# notify-keyspace-events Ex
+from redis import StrictRedis
+import time
+
+redis_conn = StrictRedis()
+pubsub = redis_conn.pubsub()
+
+
+def event_handler(msg):
+    """
+    key失效回调函数
+    ps: {'type': 'pmessage', 'pattern': b'__keyevent@0__:expired', 'channel': b'__keyevent@0__:expired', 'data': b'order_id'}
+    获取失效消息中的order_id，查询订单表，返还库存，将订单记录状态设置为`取消态`
+    :param msg:
+    :return:
+    """
+    print(msg)
+
+
+pubsub.psubscribe(**{'__keyevent@0__:expired': event_handler})
+
+
+while True:
+    pubsub.get_message()
+
+    time.sleep(0.1)
